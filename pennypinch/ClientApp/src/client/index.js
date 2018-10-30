02f32db908a81ec607c03bc34e9ebde30bee5a32
Steve Tipton <slaatipton26@gmail.com>
2018-10-30 11:32:45 -0400

    Updates to adapt to approach aligning with GraphQL/authentication

---
+++ pennypinch/ClientApp/src/client/index.js
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Root = () => {
+    return (
+        <div>
+            Auth Starter
+        </div>
+    )
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'))
--- pennypinch/ClientApp/src/index.js
+++ pennypinch/ClientApp/src/index.js
@@ -1,17 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { ApolloProvider } from 'react-apollo'
+const app = require('./server/server')
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import client from './apollo'
-
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
-)
-registerServiceWorker()
-if (module.hot) module.hot.accept()
+app.listen(3000, () => {
+    console.log('Listening')
+})
