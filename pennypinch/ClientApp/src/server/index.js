e49e94345aa12c85c3615561c4a623c48437736f
Steve Tipton <slaatipton26@gmail.com>
2018-10-30 09:46:45 -0400

    Set up the authentication

--- pennypinch/ClientApp/src/server/index.js
+++ pennypinch/ClientApp/src/server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 const graphqlHTTP = require('express-graphql')
 const gql = require('graphql-tag')
 const { buildASTSchema } = require('graphql')
+
+const UserModel = require('./model/model')
+
+mongoose.connect('mongodb://localhost:27017/pennypinch-mongodb', { useNewUrlParser: true })
+mongoose.connection.on('error', error => console.log(error))
+mongoose.Promise = global.Promise
+
+require('./auth/auth')
 
 const POSTS = [
     { author: "John Doe", body: "Hello world" },
@@ -62,6 +73,23 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const routes = require('./routes/routes')
+const secureRoute = require('./routes/secure-routes')
+
+app.use('/', routes)
+
+// We plugin our jwt strategy as middleware so only verified users can access this route
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute)
+
+// Handle errors
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({ error: err })
+})
+
 const port = process.env.PORT || 3900
 app.listen(port)
+console.log(`Server started, listening at localhost:${port}`)
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`)
---
+++ pennypinch/ClientApp/src/server/schema/schema.js
@@ -0,0 +1,8 @@
+﻿const graphql = require('graphql')
+const { GraphQLSchema } = graphql
+
+const RootQueryType = require('./types/root_query_type')
+
+module.exports = new GraphQLSchema({
+    query: RootQueryType
+})
