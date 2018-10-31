d9e7a4c5ed63ed746591e0a5b4e2d8f28aae1f41
Steve Tipton <slaatipton26@gmail.com>
2018-10-31 13:10:45 -0400

    The dashboard route

--- pennypinch/ClientApp/src/client/index.js
+++ pennypinch/ClientApp/src/client/index.js
@@ -9,6 +9,7 @@ import { Router, hashHistory, Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
+import Dashboard from './components/Dashboard'
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id,
@@ -23,6 +24,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <Route path="login" component={LoginForm} />
           <Route path="signup" component={SignupForm} />
+          <Route path="dashboard" component={Dashboard} />
         </Route>
       </Router>
     </ApolloProvider>
