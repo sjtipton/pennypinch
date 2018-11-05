c3c24a3a0792fd1a9665b4784769c10124275d8d
Steve Tipton <slaatipton26@gmail.com>
2018-11-05 13:05:08 -0500

    Move resolver for UserProfileType into ApiUserType as a nested query

--- pennypinch/ClientApp/src/server/schema/types/api_user_type.js
+++ pennypinch/ClientApp/src/server/schema/types/api_user_type.js
@@ -1,16 +1,27 @@
 const graphql = require('graphql')
+const mongoose = require('mongoose')
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLID
 } = graphql
+const UserProfileType = require('./user_profile_type')
+const { UserProfileSchema } = require('../../models/userProfile')
+const UserProfile = mongoose.model('userProfile', UserProfileSchema)
 
 const ApiUserType = new GraphQLObjectType({
   name: 'ApiUserType',
   fields: {
     id: { type: GraphQLID },
     email: { type: GraphQLString },
-    auth_token: { type: GraphQLString }
+    authToken: { type: GraphQLString },
+    profile: {
+      type: UserProfileType,
+      resolve: (parentValue, args, req) => {
+        const userid = parentValue.id
+        return UserProfile.findOne({ userid })
+      }
+    }
   }
 })
 
--- pennypinch/ClientApp/src/server/schema/types/root_query_type.js
+++ pennypinch/ClientApp/src/server/schema/types/root_query_type.js
@@ -1,14 +1,10 @@
 const graphql = require('graphql')
-const mongoose = require('mongoose')
 const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString
 } = graphql
 const ApiUserType = require('./api_user_type')
-const UserProfileType = require('./user_profile_type')
-const { UserProfileSchema } = require('../../models/userProfile')
-const UserProfile = mongoose.model('userProfile', UserProfileSchema)
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -21,14 +17,6 @@ const RootQueryType = new GraphQLObjectType({
       },
       resolve: (parentValue, args, req) => {
         return req.user
-      }
-    },
-    userProfile: {
-      type: UserProfileType,
-      resolve: (parentValue, args, req) => {
-        const userid = req.user.id
-        // TODO attach resolver to helper so it can be reused throughout app
-        return UserProfile.findOne({ userid })
       }
     }
   }
